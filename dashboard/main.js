608b1f1c158cd0b827c773af058b6d76ac8c1b06
earthperson <ponomarev.dev@gmail.com>
2014-01-03 19:30:31 +0400

    Added some dynamics

---
+++ dashboard/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.btn-primary:first').click(function() {
+		var row = $('.col-md-8 .row:first').clone(true).appendTo('.col-md-8');
+		$('.form-group', row).removeClass('hidden');
+	});
+	$('.btn-danger').on('click', function() {
+		if($('.btn-danger').size() > 1) {
+			$(this).parents('.row:eq(1)').remove();
+		}
+	});
+});
