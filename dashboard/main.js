a9e77562563a89a5508434129c1eae4e2ed5d7c2
earthperson <ponomarev.dev@gmail.com>
2014-01-03 21:36:12 +0400

    Some dashboard improvements

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,6 +1,8 @@
 $(function() {
 	$('.btn-primary:first').click(function() {
-		var row = $('.col-md-8 .row:first').clone(true).appendTo('.col-md-8');
+		var row = $('.col-md-8 .row:first').clone(true);
+		$('input', row).val('');
+		row.appendTo('.col-md-8');
 		$('.form-group', row).removeClass('hidden');
 	});
 	$('.btn-danger').on('click', function() {
