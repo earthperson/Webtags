a9912b42cab36b8179045f1bb3ff5e7c5f0da639
earthperson <ponomarev.dev@gmail.com>
2014-01-04 16:35:00 +0400

    In the dashboard: Bulk actions for the checkboxes added

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,13 +1,32 @@
 $(function() {
 	$('.btn-primary:first').click(function() {
 		var row = $('.col-md-8 .row:first').clone(true);
+		$('.btn-group', row).remove();
 		$('input', row).val('');
+		$('input', row).removeProp('checked');
+		$('.row:first', row).removeClass('hidden');
 		row.appendTo('.col-md-8');
-		$('.form-group', row).removeClass('hidden');
 	});
-	$('.btn-danger').on('click', function() {
+	$('.btn-danger:not(.dropdown-toggle)').on('click', function() {
 		if($('.btn-danger').size() > 1) {
 			$(this).parents('.row:eq(1)').remove();
+		}
+	});
+	$('.btn-group .dropdown-menu a').on('click', function() {
+		var index = $(this).parent().index();
+		var checkboxes = $(this).closest('.col-md-8').find(':checkbox');
+		if (index == 0) { // Check all
+			checkboxes.prop('checked', true);
+		}
+		else if (index == 1) { // Uncheck all
+			checkboxes.prop('checked', false);
+		}
+		else if (index == 3) { // Remove checked
+			$($(this).closest('.col-md-8').children('.row:not(:first-child)')).each(function(index, Element) {
+				if ($(this).has(':checked').length > 0) {
+					$(this).remove();
+				}
+			});
 		}
 	});
 });
