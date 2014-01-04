6b92a70c8debc9ab6bd318a083c58c54b287bc12
earthperson <ponomarev.dev@gmail.com>
2014-01-04 14:38:06 +0400

    In the dashboard: added favicon.ico, version, checkbox for export type, some other improvements. Added some initial code for rendering.

---
+++ render.js
@@ -0,0 +1,14 @@
+(function() {
+	var canvas = document.getElementById('canvas');
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
+		ctx.fillStyle = "rgb(200,0,0)";
+		ctx.fillRect (10, 10, 55, 50);
+		
+		ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+		ctx.fillRect (30, 30, 55, 50);
+	}
+	else {
+		// canvas-unsupported code here
+	}
+})();
