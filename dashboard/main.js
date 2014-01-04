295bd7fd59a8b9d0d72cf4708cd9547a59d1b67e
earthperson <ponomarev.dev@gmail.com>
2014-01-04 21:51:36 +0400

    In the dashboard added numbering of the checkboxes, some improvements

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,21 +1,37 @@
 $(function() {
+	$.fn.setCheckboxesIndex = function () {
+		this.each(function(index) {
+			$(this).parent().contents().filter(function() {
+			    return this.nodeType === 3;
+			}).remove();
+			$(this).after('#'+(++index));
+		});
+		return this;
+	};
 	$('.btn-primary:first').click(function() {
-		var row = $('.col-md-8 .row:first').clone(true);
+		var row = $('.col-md-8 .row:first').clone(true),
+			index = $('.btn-danger').size(),
+			checkbox = $(':checkbox', row);
 		$('.btn-group', row).remove();
 		$('input:not(:checkbox)', row).val('');
-		$(':checkbox', row).prop('checked', false);
+		checkbox.prop('checked', false);
+		checkbox.parent().contents().filter(function() {
+		    return this.nodeType === 3;
+		}).remove();
+		checkbox.after('#'+index);
 		$('.row:first', row).removeClass('hidden');
 		row.appendTo('.col-md-8');
 	});
 	$('.btn-danger:not(.dropdown-toggle)').on('click', function() {
 		if($('.btn-danger').size() > 1) {
 			$(this).parents('.row:eq(1)').remove();
+			$(':checkbox', $('.col-md-8')).setCheckboxesIndex();
 		}
 	});
 	$('.btn-group .dropdown-menu a').click(function(event) {
 		event.preventDefault();
-		var index = $(this).parent().index();
-		var checkboxes = $(this).closest('.col-md-8').find(':checkbox');
+		var index = $(this).parent().index(),
+			checkboxes = $(this).closest('.col-md-8').find(':checkbox');
 		if (index == 0) { // Check all
 			checkboxes.prop('checked', true);
 		}
@@ -28,6 +44,7 @@ $(function() {
 					$(this).remove();
 				}
 			});
+			$(':checkbox', $('.col-md-8')).setCheckboxesIndex();
 		}
 	});
 });
