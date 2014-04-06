8108d0e7da42c8eea6a878ae54eca68cb3b88629
earthperson <ponomarev.dev@gmail.com>
2014-04-06 23:06:35 +0400

    Fixed ecdda87cfaeeaac79273a34f1d65ba6f014a3fbd commit

--- dashboard/main.js
+++ dashboard/main.js
@@ -201,7 +201,7 @@ $(function() {
 		$('#modalImport textarea').text('{"items":[{"label":"GitHub","url":"https://github.com/earthperson/Webtags"},{"label":"Webtags","url":"http://earthperson.github.io/Webtags/"},{"label":"Dashboard","url":"http://earthperson.github.io/Webtags/dashboard/"},{"label":"Author website","url":"http://earthperson.info/en/"},{"label":"Author website 2","url":"http://dev.earthperson.info/en/"}],"type":"rounded","width":500,"height":350,"border":true,"donate":false,"grid":false}');
 	});
 	$('.modal .btn-primary').click(function() {
-		var data = $('#modalImport textarea').val(), c;
+		var data = $('#modalImport textarea').val();
 		try {
 			data = JSON.parse($.trim(stripTags(data, /<canvas[^>]+>.*?<\/[^>]+>/gi).replace(/(?:\/\*(?:[\s\S]*?)\*\/)|(?:([\s;])+\/\/(?:.*)$)/gm, '$1').replace(/new Webtags\(((.*\s*)*)\);/i, '$1')));
 			$('#modalImport').modal('hide');
