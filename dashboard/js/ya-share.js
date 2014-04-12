ef50475f5ce2216962244ef71bf941d8ea805ce8
earthperson <ponomarev.dev@gmail.com>
2014-04-12 17:13:25 +0400

    Update dashboard Added Yandex.Share

---
+++ dashboard/js/ya-share.js
@@ -0,0 +1,13 @@
+new Ya.share({
+    element: 'ya_share',
+    l10n: 'en',
+    elementStyle: {
+        'type': 'button',
+        'quickServices': ['facebook','twitter','linkedin','gplus']
+    },
+    popupStyle: {
+        blocks: {
+        	"Share with friends": ['facebook','twitter','linkedin','gplus']
+        }
+    }
+});
