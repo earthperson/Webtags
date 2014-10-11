c92e2556cac9b3400fd1d4069baa6dcc39e0e38f
earthperson <ponomarev.dev@gmail.com>
2014-10-11 10:46:56 +0400

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*!\n' + 
+					' * <%= pkg.name %> v<%= pkg.version %>-pl, Dashboard for <%= pkg.name %> v<%= pkg.dashboard.version %>-pl\n' +
+					' * <%= pkg.name %> GitHub page (source code and links): (<%= pkg.homepage %>)\n' +
+					' * <%= pkg.name %> website: (<%= pkg.website %>)\n' +
+					' * Dashboard for <%= pkg.name %>: (<%= pkg.dashboard.homepage %>)\n' +
+					' * Author website: (<%= pkg.author.website %>)\n' +
+					' *\n' +
+					' * Copyright (c) 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+					' * Licensed under the <%= pkg.license.type %>: (<%= pkg.license.url %>)\n' + 
+					' */\n'
+			},
+			build: {
+				src: 'render.js',
+				dest: 'render.min.js'
+			}
+		},
+		jshint: {
+			all: ['Gruntfile.js', 'render.js', 'dashboard/main.js']
+		}
+	});
+	
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	
+	// Default task(s).
+	grunt.registerTask('default', ['uglify', 'jshint']);
+};
---
+++ render.js
@@ -0,0 +1,298 @@
+/*!
+ * Webtags v0.4.4-pl, Dashboard for Webtags v1.0.16-pl
+ * Webtags GitHub page (source code and links): (https://github.com/earthperson/Webtags)
+ * Webtags website: (http://earthperson.github.io/Webtags/)
+ * Dashboard for Webtags: (http://earthperson.github.io/Webtags/dashboard/)
+ * Author website: (http://earthperson.info)
+ *
+ * Copyright (c) 2013-2014 Dmitry Ponomarev (email: ponomarev.dev@gmail.com)
+ * Licensed under the MIT License: (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/*jshint -W056 */
+/*jshint -W069 */
+function Webtags (properties) {
+	if (properties instanceof Object && properties['items']) {
+		for (var a in properties) {
+			this.properties[a] = properties[a];
+		}
+		this.canvas.render();
+	}	
+}
+Webtags.prototype.properties = {};
+Webtags.prototype.canvas = null;
+(function(Webtags) {
+	
+	function Canvas() {
+		var properties = {
+			id: "webtags",
+			type: "rounded", // "square"
+			width: 500,
+			height: 350,
+			border: true,
+			grid: false,
+			style: {
+				border: "1px solid #5e8cc2"
+			}
+		}, a;
+		for (a in properties) {
+			this.properties[a] = properties[a];
+		}
+	}
+	Canvas.prototype = new Webtags();
+	Canvas.prototype.items = [];
+	Canvas.prototype.hover = null;
+	Canvas.POWERED_BY = {
+		LABEL: "Powered by Webtags v0.4.4-pl",
+		URL: "https://github.com/earthperson/Webtags",
+		FONT: "10px Helvetica"
+	};
+	Canvas.prototype.poweredBy = {
+		x: null,
+		y: null
+	};
+	Canvas.prototype.element = null;
+	Canvas.prototype.getMousePosition = function(canvas, e) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: e.clientX - rect.left,
+			y: e.clientY - rect.top
+		};
+	};
+	// Check if the mouse is over the webtag label or powered by and change cursor style
+	Canvas.prototype.onMousemove = function(e) {
+		var mousePosition = Canvas.prototype.getMousePosition(Canvas.prototype.element, e), x = mousePosition.x, y = mousePosition.y, a, o;
+		document.body.style.cursor = Canvas.prototype.hover = null;
+		for (a in Canvas.prototype.items) {
+			o = Canvas.prototype.items[a].text;
+			// Is the mouse over the webtag label?
+			if (x >= parseInt(o.x+o.translating.x) && x <= (parseInt(o.x+o.translating.x) + parseInt(o.width)) && y >= parseInt(o.y+o.translating.y) && y <= (parseInt(o.y+o.translating.y) + parseInt(o.height))) {
+				document.body.style.cursor = 'pointer';
+				Canvas.prototype.hover = Canvas.prototype.items[a].item.url;
+				break;
+			}
+		}
+		if(Canvas.prototype.properties.border) {
+			o = Canvas.prototype.poweredBy;
+			// Is the mouse over the powered by?
+			if (x >= o.x && x <= Canvas.prototype.properties.width && y >= o.y-1 && y <= Canvas.prototype.properties.height) {
+				document.body.style.cursor = 'pointer';
+				Canvas.prototype.hover = Canvas.POWERED_BY.URL;
+			}
+		}
+	};
+	Canvas.prototype.onClick = function(e) {
+		if (Canvas.prototype.hover)  {
+			window.open(Canvas.prototype.hover);
+		}
+	};
+	Canvas.prototype.render = function() {
+		var canvas = Canvas.prototype.element = document.getElementById(this.properties.id);
+		canvas.width = this.properties.width;
+		canvas.height = this.properties.height;
+		canvas.style.border = this.properties.border ? this.properties.style.border : 'none';
+		if (canvas.getContext) {
+			var context = canvas.getContext('2d'), i = 0, l = this.properties.items.length;
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			Tag.prototype.count = 0;
+			Tag.prototype.line = {
+				width: 0,
+				count: 0
+			};
+			// Add mouse listeners
+			canvas.addEventListener('mousemove', Canvas.prototype.onMousemove, false);
+			canvas.addEventListener('click', Canvas.prototype.onClick, false);
+			// Render tags
+			for(; i < l; i++) {
+				Canvas.prototype.items.push(
+					new (this.properties.type == 'square' ? SquareTag : RoundedTag)(context, this.properties.items[i])
+				);
+			}
+			context.fillStyle = this.properties.style.border.match(/#\w{3,}$/) || '#5e8cc2';
+			// Render powered by
+			if (this.properties.border) {
+				context.font = Canvas.POWERED_BY.FONT;
+				this.poweredBy.x = parseInt(Canvas.prototype.properties.width - context.measureText(Canvas.POWERED_BY.LABEL).width);
+				this.poweredBy.y = parseInt(Canvas.prototype.properties.height - parseInt(Canvas.POWERED_BY.FONT));
+				context.fillText(Canvas.POWERED_BY.LABEL, this.poweredBy.x, this.poweredBy.y-1);
+			}
+		}
+		else {
+			// Canvas-unsupported code here
+		}
+	};
+	Canvas.prototype.toString = function() {
+		this.render();
+	};
+	
+	function Tag() {
+		
+	}
+	Tag.prototype.properties = {
+		width: 100,
+		height: 32,
+		context: {
+			fillStyle: "#5e8cc2",
+			textBaseline: "top",
+			font: "12px Arial",
+			strokeStyle: "#5e8cc2",
+			lineWidth: 2, 
+			lineJoin: "round",
+			lineCap: "round",
+			shadowOffsetX: 2,
+			shadowOffsetY: 2,
+			shadowBlur: 2,
+			shadowColor: "rgba(54, 111, 179, 0.4)"
+		}
+	};
+	Tag.prototype.context = null;
+	Tag.prototype.item = null;
+	Tag.prototype.text = null;
+	Tag.prototype.getRandomFactor = function() {
+		return (Math.random() * 10) + 1;
+	};
+	Tag.prototype.offsetX = function() {
+		var w = Tag.prototype.properties.width + this.edgeWidth();
+		return parseFloat((this.count % Math.floor(Canvas.prototype.properties.width / w)) * w);
+	};
+	Tag.prototype.offsetY = function() {
+		var w = Tag.prototype.properties.width + this.edgeWidth();
+		return Math.floor(this.count / Math.floor(Canvas.prototype.properties.width / w)) * (Tag.prototype.properties.height+8);
+	};
+	Tag.prototype.translateX = function() {
+		if(this.line.width + this.text.width > (Canvas.prototype.properties.width - this.edgeWidth())) {
+			this.line.width = 0;
+			this.line.count++;
+			return 0;
+		}
+		else {
+			return this.line.width;
+		}
+	};
+	Tag.prototype.edgeWidth = function() {
+		return Canvas.prototype.properties.type == 'rounded' ? 16+Tag.prototype.properties.height+2 : 16+23+2;
+	};
+	Tag.prototype.translateY = function() {
+		return (Tag.prototype.properties.height+8) * Tag.prototype.line.count;
+	};
+	Tag.prototype.render = function() {
+		var k = this.getRandomFactor(), properties = Canvas.prototype.properties, r, a;
+		if (properties['tag'] instanceof Object) {
+			for (a in properties.tag) {
+				if(a === 'context' && properties.tag.context instanceof Object) {
+					for (a in properties.tag.context) {
+						this.properties.context[a] = properties.tag.context[a];
+					}
+					continue;
+				}
+				this.properties[a] = properties.tag[a];
+			}
+		}
+		for(a in this.properties.context) {
+			this.context[a] = this.properties.context[a];
+		}
+		r = Math.floor(Tag.prototype.properties.height/2);
+		this.text = {
+			x: 34,
+			y: r,
+			translating: {
+				x: 0,
+				y: 0
+			}
+		};
+		this.text.width = this.context.measureText(this.item.label).width;
+		this.text.height = parseInt(this.context.font);
+		this.context.save();
+		if(Canvas.prototype.properties.grid) {
+			this.text.translating.x = this.offsetX();
+			this.text.translating.y = this.offsetY();
+		}
+		else {
+			this.text.translating.x = this.translateX();
+			this.text.translating.y = this.translateY();
+		}
+		this.context.translate(this.text.translating.x, this.text.translating.y);
+		this.context.beginPath();
+		this.context.arc(26,4+r,4,0,Math.PI*2);
+		this.context.moveTo(4,18);
+		this.context.bezierCurveTo(2+k,10+k,8,10,10,14);
+		this.context.bezierCurveTo(13,23,16,17,27,4+r);
+		this.context.moveTo(2,26);
+		k = this.getRandomFactor();
+		this.context.quadraticCurveTo(2+k,18+k,12,22);
+		this.context.quadraticCurveTo(18,28,27,4+r);
+		this.context.stroke();
+		this.context.fillText(this.item.label,this.text.x,this.text.y-2);
+		this.context.restore();
+	};
+	
+	function RoundedTag(context, item) {
+		this.context = context;
+		this.item = item;
+		Tag.prototype.render.call(this);
+		this.render();
+		Tag.prototype.line.width += Math.floor(this.edgeWidth()+this.text.width);
+		Tag.prototype.count++;
+	}
+	RoundedTag.prototype = new Tag();
+	RoundedTag.prototype.render = function() {
+		var r = Math.floor(Tag.prototype.properties.height/2);
+		this.context.save();
+		if(Canvas.prototype.properties.grid) {
+			this.context.translate(this.offsetX(), this.offsetY());
+		}
+		else {
+			this.context.translate(this.translateX(), this.translateY());
+		}
+		this.context.beginPath();
+		this.context.arc(16+r,4+r,r,0.5*Math.PI,1.5*Math.PI);
+		if(Canvas.prototype.properties.grid) {
+			this.context.lineTo(16+r+Tag.prototype.properties.width,4);
+			this.context.arc(16+r+Tag.prototype.properties.width,4+r,r,1.5*Math.PI,0.5*Math.PI);
+		}
+		else {
+			this.context.lineTo(16+r+this.text.width,4);
+			this.context.arc(16+r+this.text.width,4+r,r,1.5*Math.PI,0.5*Math.PI);
+		}
+		this.context.closePath();
+		this.context.stroke();
+		this.context.restore();
+	};
+	
+	function SquareTag(context, item) {
+		this.context = context;
+		this.item = item;
+		Tag.prototype.render.call(this);
+		this.render();
+		Tag.prototype.line.width += Math.floor(16+23+2+this.text.width);
+		Tag.prototype.count++;
+	}
+	SquareTag.prototype = new Tag();
+	SquareTag.prototype.render = function() {
+		this.context.save();
+		if(Canvas.prototype.properties.grid) {
+			this.context.translate(this.offsetX(), this.offsetY());
+		}
+		else {
+			this.context.translate(this.translateX(), this.translateY());
+		}
+		this.context.beginPath();
+		this.context.moveTo(16,4+Math.floor(Tag.prototype.properties.height/2));
+		this.context.lineTo(23,4);
+		if(Canvas.prototype.properties.grid) {
+			this.context.lineTo(23+Tag.prototype.properties.width,4);
+			this.context.lineTo(23+Tag.prototype.properties.width,Tag.prototype.properties.height+4);
+		}
+		else {
+			this.context.lineTo(16+23+this.text.width,4);
+			this.context.lineTo(16+23+this.text.width,Tag.prototype.properties.height+4);
+		}
+		this.context.lineTo(23,Tag.prototype.properties.height+4);
+		this.context.closePath();
+		this.context.stroke();
+		this.context.restore();
+	};
+	
+	Webtags.prototype.canvas = new Canvas();
+	
+})(Webtags);
